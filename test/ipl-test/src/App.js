be180a2ede6e1b118bc6353717307697bb32ae14
koundinyaatchyutuni <20131a4407@gmail.com>
2026-03-20 22:57:08 +0530

    initial app with login front end

--- test/ipl-test/src/App.js
+++ test/ipl-test/src/App.js
@@ -1,32 +1,23 @@
 import './App.css';
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        Login / >
-        <
-        /div>
+    return (
+        <div className="App">
+            <Login />
+        </div>
     );
 }
 
 function Login() {
-    return ( <
-        div id = "log_in" >
-        <
-        h1 > Log In < /h1> <
-        input type = "text"
-        placeholder = "Username" / >
-        <
-        br / >
-        <
-        input type = "password"
-        placeholder = "Password" / >
-        <
-        br / >
-        <
-        button > Log In < /button> <
-        /div>
+    return (
+        <div id="log_in">
+            <h1>Log In</h1>
+            <input type="text" placeholder="Username" />
+            <br />
+            <input type="password" placeholder="Password" />
+            <br />
+            <button>Log In</button>
+        </div>
     );
 }
 
